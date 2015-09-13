2f6a4e3e0c12b461159a2175ab6b8a1cf3af64f5
Ali Sinan Köksal <alisinan@gmail.com>
2015-09-13 15:27:06 -0700

    Separate code for printing signed directed networks

---
+++ tps-core/src/main/scala/tps/SignedDirectedNetworkPrinter.scala
@@ -0,0 +1,43 @@
+package tps
+
+import GraphSolutions._
+import UndirectedGraphs._
+
+object SignedDirectedNetworkPrinter {
+  def print(g: AmbiguousGraphSolution): String = {
+    val sb = new StringBuffer()
+    val fields = List(
+      "source",
+      "target",
+      "left-right-act",
+      "left-right-inh",
+      "right-left-act",
+      "right-left-inh"
+    )
+    sb append fields.mkString("\t")
+    sb append "\n"
+
+    for ((Edge(Vertex(v1), Vertex(v2)), ess) <- g) {
+      assert(!ess.isEmpty)
+      if (!noDirection(ess)) {
+        val lra = ess contains ActiveEdge(Forward, Activating)
+        val lri = ess contains ActiveEdge(Forward, Inhibiting)
+        val rla = ess contains ActiveEdge(Backward, Activating)
+        val rli = ess contains ActiveEdge(Backward, Inhibiting)
+      
+        val row = List(
+          v1,
+          v2,
+          lra,
+          lri,
+          rla,
+          rli
+        )
+        sb append row.mkString("\t")
+        sb append "\n"
+      }
+    }
+
+    sb.toString
+  }
+}
