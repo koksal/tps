fdcff1572c3712a6657358c1654bdadc998ab824
Ali Sinan Köksal <alisinan@gmail.com>
2017-06-25 12:58:56 -0700

    clean up unused random graph generation code

--- tps-core/src/main/scala/tps/simulation/RandomGraphGenerator.scala
+++ tps-core/src/main/scala/tps/simulation/RandomGraphGenerator.scala
@@ -46,40 +46,4 @@ object RandomGraphGenerator {
     UndirectedGraph(V, E, Set(src))
   }
 
-  def generateRandomGraphFromSourceGraph(sourceGraph: UndirectedGraph,
-                          maxNodeLimit: Int): UndirectedGraph = {
-    assert(sourceGraph.sources.size <= maxNodeLimit)
-
-    var generatedGraph = UndirectedGraph(sourceGraph.sources, Set.empty,
-      sourceGraph.sources)
-
-    try {
-      while (generatedGraph.V.size < maxNodeLimit) {
-        // find a random incident edge to add while respecting max degree limit.
-        val candidates = extensionCandidates(generatedGraph, sourceGraph)
-        if (candidates.isEmpty) throw CannotExtendException
-
-        val edgeToAdd = candidates.toIndexedSeq(random.nextInt(candidates.size))
-        generatedGraph = UndirectedGraph(
-          generatedGraph.V ++ Set(edgeToAdd.v1, edgeToAdd.v2),
-          generatedGraph.E + edgeToAdd,
-          generatedGraph.sources)
-      }
-      generatedGraph
-    } catch {
-      case CannotExtendException => generatedGraph
-    }
-  }
-
-  private def extensionCandidates(graphToExtend: UndirectedGraph,
-                                  sourceGraph: UndirectedGraph): Set[Edge] = {
-    graphToExtend.V.flatMap { v =>
-      sourceGraph.incidentEdges(v).filter { e =>
-        // only take edges that do not exist and that won't exceed max indegree
-        !graphToExtend.E.contains(e) &&
-          graphToExtend.incidentEdges(e.v1).size < MAX_NODE_DEGREE &&
-          graphToExtend.incidentEdges(e.v2).size < MAX_NODE_DEGREE
-      }
-    }
-  }
 }
--- tps-core/src/test/scala/tps/simulation/RandomGraphGeneratorTest.scala
+++ tps-core/src/test/scala/tps/simulation/RandomGraphGeneratorTest.scala
@@ -18,16 +18,4 @@ class RandomGraphGeneratorTest extends FunSuite with Matchers {
     generated.E.size should equal (5)
   }
 
-  test("random graph from existing graph contains source and has right size") {
-    val sourceGraph = UndirectedGraphParser.run(testFile("network.tsv")).copy(
-        sources = Set(Vertex("A")))
-    val maxNodeLimit = 3
-    val generated = RandomGraphGenerator.generateRandomGraphFromSourceGraph(
-      sourceGraph,
-      maxNodeLimit)
-
-    generated.V.size should equal (3)
-    generated.V should contain (Vertex("A"))
-  }
-
 }
