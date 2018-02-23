62315eb5248c06c98dfe095f6f83ab6af7c94ba6
Anthony Gitter <gitter@biostat.wisc.edu>
2018-02-23 14:08:22 -0600

    Move network randomization scripts

--- pcsf/run_net_rand.sh
+++ pcsf/run_net_rand.sh
@@ -2,7 +2,7 @@
 
 NETWORK_FILE='data/networks/directed-pin-with-resource-edges.tsv'
 
-python scripts/background_network_randomization/generate_randomized_networks.py \
+python pcsf/generate_randomized_networks.py \
   --network $NETWORK_FILE \
   --outdir . \
   --copies 10
